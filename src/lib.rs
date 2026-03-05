6bdfcfab305eaff54452dff1b3f36f98255a1754
vincent <vin.krae@gmail.com>
2026-03-05 20:01:54 +0100

    Temporary: Added Plans for future APIs

--- src/lib.rs
+++ src/lib.rs
@@ -23,5 +23,20 @@ pub mod protocols;
 // Key management
 pub(crate) mod keys;
 
+/*
+Desired primitives / functionalities:
+    All functions return a result
+- Sign (keygen()->sk,pk; sign(sk, m)->sig; vrfy(pk, m, sig)->bool)
+- KEM (keygen()->sk,pk; encaps(pk)->ct,shk; decaps(sk, ct)->shk)
+- NIKE (Non-interactive key-exchange)
+    (keygen()->sk,pk; shared_key(sk, pk)->shk)
+- AEAD (keygen()->sk; enc(sk, m, ad, nonce)->ct, tag)??? key-commitment?
+- KDF/PRF/Dual-PRF???
+- Cyptographic Hash / XOF (extend(seed)->rand)
+*/
+// One Agent per application to allow transitions of keys
+// KDF produce n bytes symmetric keys
+// Symmetric keys have transitions from untyped to application specific
+
 static RNG: LazyLock<RwLock<ChaCha20Rng>> =
     LazyLock::new(|| RwLock::new(ChaCha20Rng::from_os_rng()));
